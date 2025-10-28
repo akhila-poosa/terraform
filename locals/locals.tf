ca4b46e67511caf3ec22919ba977d44e32180a5a
akhilapoosaa <akhila.1829@gmail.com>
2025-10-29 00:38:02 +0530

    updated

--- locals/locals.tf
+++ locals/locals.tf
@@ -1,7 +1,7 @@
 locals{
     instance_type = "t3.micro"
     common_name = "${var.project}-${var.environment}" 
-    ami_id = data.aws_ami.joindevops.id
+    ami_id = data.aws_ami.devops.id
     ec2_tags = merge(
       var.common_tags,
       {
