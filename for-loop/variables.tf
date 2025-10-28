435354f3a7de5019fc2022c20c01655531c45754
akhilapoosaa <akhila.1829@gmail.com>
2025-10-28 12:48:33 +0530

    updated

---
+++ for-loop/variables.tf
@@ -0,0 +1,11 @@
+variable "instances" {
+    default = [ "mongodb", "redis" ]
+}
+
+variable "zone_id" {
+    default = "Z08544841KFKOJ4HN9E5S"
+}
+
+variable "domain_name" {
+    default = "zealful.space"
+}
