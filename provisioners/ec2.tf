5f150cab898f084e8124e92889c1e62aa9dad19b
akhilapoosaa <akhila.1829@gmail.com>
2025-10-29 16:36:50 +0530

    updated

--- provisioners/ec2.tf
+++ provisioners/ec2.tf
@@ -1,65 +1,69 @@
 resource "aws_instance" "terraform" {
-    ami = "ami-09c813fb71547fc4f"
-    instance_type = "t3.micro"
-    vpc_security_group_ids = [aws_security_group.allow_all.id]
-    tags = {
-        Name = "terraform"
-        Terraform = "true"
-    }
-}
+  ami                    = "ami-09c813fb71547fc4f"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+  tags = {
+    Name      = "terraform"
+    Terraform = "true"
+  }
 
+  # Run a local command after instance creation
   provisioner "local-exec" {
-      command = "echo ${self.private_ip} > inventory"
-      on_failure = continue
-    }
+    command    = "echo ${self.private_ip} > inventory"
+    on_failure = continue
+  }
 
+  # Run local command during destroy
   provisioner "local-exec" {
-      command = "echo Instance is destroyed"
-      when    = destroy
-    }
+    command = "echo Instance is destroyed"
+    when    = destroy
+  }
 
+  # SSH connection details for remote provisioners
   connection {
-      type     = "ssh"
-      user     = "ec2-user"
-      password = "DevOps321"
-      host     = self.public_ip
-    }
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = self.public_ip
+  }
 
+  # Run commands on the instance after creation
   provisioner "remote-exec" {
-      inline = [
-        "sudo dnf install nginx -y",
-        "sudo systemctl start nginx"
-      ]
-    }
+    inline = [
+      "sudo dnf install nginx -y",
+      "sudo systemctl start nginx"
+    ]
+  }
 
+  # Run commands on the instance during destroy
   provisioner "remote-exec" {
-      inline = [
-        "sudo systemctl stop nginx",
-        "echo 'successfully stopped nginx server' "
-      ]
-      when = destroy
-    }
+    when = destroy
+    inline = [
+      "sudo systemctl stop nginx",
+      "echo 'successfully stopped nginx server'"
+    ]
+  }
 }
 
-resource "aws_security_group" "allow_all" {  #allow-all name is for terraform reference 
-  name   = "allow-all"  #sg-name we see on aws
+# Security Group
+resource "aws_security_group" "allow_all" {
+  name = "allow-all"
 
   egress {
-    from_port        = 0 # from port 0 to to port 0 means all ports
-    to_port          = 0 
-    protocol         = "-1" # -1 means all protocols
-    cidr_blocks      = ["0.0.0.0/0"] # anywhere on the internet
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port        = 0 # from port 0 to to port 0 means all ports
-    to_port          = 0 
-    protocol         = "-1" # -1 means all protocols
-    cidr_blocks      = ["0.0.0.0/0"] # anywhere on the internet
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
-    Name = "allow-all" # tags for user display
+    Name = "allow-all"
   }
-
 }
